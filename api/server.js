ade6f38488853e816825d5bd3e571198147548ba
Claire List <claire.list2501@gmail.com>
2022-01-12 18:42:55 -0700

    server scaffolded

--- api/server.js
+++ api/server.js
@@ -6,9 +6,23 @@ const server = express();
 //server.use(cors());
 server.use(express.json());
 // global middlewares and the user's router need to be connected here
+//server.use('/api/hubs', hubsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+});
+
+server.use('*', (req, res) => {
+  // catch all 404 errors middleware
+  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
+});
+
+//specified error message not caught by above OR any other catcher !
+server.use((err, req, res, next) => { // eslint-disable-line
+  console.log('disaster!')
+  res.status(err.status || 500).json({
+    message: `The Horror: ${err.message}`,
+  });
 });
 
 module.exports = server;
