9304a90ca16382b732d698336ee80918f833fb58
Claire List <claire.list2501@gmail.com>
2022-01-12 19:19:25 -0700

    logger middleware built !

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,35 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+
+  const timestamp = new Date().toLocaleString();
+  const methodr = req.method;
+  const url = req.originalUrl;
+
+  console.log(`Method ${methodr} made to ${url} at local time: ${timestamp}`);
+  next(); //call next so we havae barebones middleware !
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateId middleware !');
+  next(); //call next so we havae barebones middleware !
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUser middleware !');
+  next(); //call next so we havae barebones middleware !
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validatePost middleware !');
+  next(); //call next so we havae barebones middleware !
 }
 
-// do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
--- api/server.js
+++ api/server.js
@@ -1,28 +1,35 @@
+//// DATA SECTION
 const express = require('express');
 const server = express();
 // const cors = require('cors');
-// const hubsRouter = require('patth');
+//const usersRouter = require('./users/users-router');
 
+const {logger} = require('./middleware/middleware');
+
+//// LOGIC SECTION
 //server.use(cors());
 server.use(express.json());
 // global middlewares and the user's router need to be connected here
-//server.use('/api/hubs', hubsRouter);
+//server.use('/api/users', usersRouter);
+
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-server.use('*', (req, res) => {
-  // catch all 404 errors middleware
-  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
-});
+// server.use('*', (req, res) => {
+//   // catch all 404 errors middleware
+//   res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
+// });
 
-//specified error message not caught by above OR any other catcher !
-server.use((err, req, res, next) => { // eslint-disable-line
-  console.log('disaster!')
-  res.status(err.status || 500).json({
-    message: `The Horror: ${err.message}`,
-  });
-});
+// //specified error message not caught by above OR any other catcher !
+// server.use((err, req, res, next) => { // eslint-disable-line
+//   console.log('disaster!')
+//   res.status(err.status || 500).json({
+//     message: `The Horror: ${err.message}`,
+//   });
+// });
 
+//RETURN PSEUD EXPORT SECTIONR
 module.exports = server;
